dc0d568d813c50cf60bd0a3b00ebe81dfb1de895
rquinterosv <rafael.quinterosv@gmail.com>
2022-06-07 22:28:42 -0400

    Se agrega scaffold de categorias

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_021543) do
+ActiveRecord::Schema.define(version: 2022_06_08_022731) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_categories_on_movie_id"
+  end
 
   create_table "characters", force: :cascade do |t|
     t.string "image"
@@ -44,5 +52,6 @@ ActiveRecord::Schema.define(version: 2022_06_08_021543) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "movies"
   add_foreign_key "movies", "characters"
 end
